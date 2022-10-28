261fe79cd606edf3053ea9fd27058cc5853941d9
Pedro <pedrocpt2001@icloud.com>
2022-10-28 12:04:19 -0400

    pointed app to splash screen

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import SplashScreen from './screens/SplashScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SplashScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '5%',
   },
 });
