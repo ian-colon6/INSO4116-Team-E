224bf617d12aa756734e6d7ce7528398746ea486
Pedro <pedrocpt2001@icloud.com>
2022-10-29 01:45:16 -0400

    added navigation

--- frontend/App.js
+++ frontend/App.js
@@ -2,13 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React from 'react';
 import SplashScreen from './screens/SplashScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Linking from 'expo-linking';
+import MainScreen from './screens/MainScreen';
+
+const urlConfig = {
+  screens: {
+    MainScreen: 'main_screen'
+  }
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SplashScreen />
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer linking={{
+    //   prefixes: [Linking.createURL('ims://app')],
+    //   urlConfig
+    // }}>
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name='Welcome' component={SplashScreen}/>
+          <Stack.Screen name='Main' component={MainScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
